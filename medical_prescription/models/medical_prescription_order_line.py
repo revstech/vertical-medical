4ed0a8780568bd5f3bdd79bb5b88a73ecf47f845
Brett Wood <bwood@laslabs.com>
2017-04-28 19:30:53 -0700

    [FIX] Move default rx_line name to medical_prescription. * Moved from sale_medical_prescription

--- medical_prescription/models/medical_prescription_order_line.py
+++ medical_prescription/models/medical_prescription_order_line.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright 2004 Tech-Receptives
 # Copyright 2016 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -32,6 +33,7 @@ class MedicalPrescriptionOrderLine(models.Model):
     name = fields.Char(
         required=True,
         default=lambda s: s._default_name(),
+        readonly=True,
     )
 
     is_expired = fields.Boolean(
