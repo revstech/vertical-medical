dd96b2cbbfeed1a3c1f3c1f0d830d8d50b6b7c27
Brett Wood <bmwood@lton.io>
2017-05-16 20:37:00 -0700

    [10.0][IMP] sale_stock_medical_prescription_us forward port (#201)
    * [IMP] sale_stock_medical_prescription_us: Forward port.
    * Forward port of 0814e2160290ce23888b6814c71bfea01162eaad
    * Update readme
    * Update manifest
    * Add demo data
    * Refactor tests to use demo data
    * Update headers
    * Refactor models
    * Fix res_company bug not raising errors
    * Add post_init_hook for medication templates
    * Make changes compatible with 10.0
    
    * [FIX] sale_stock_medical_prescription_us: Fix rx_line.
    * Fix division by 0 errors in rx_line

--- sale_stock_medical_prescription_us/models/procurement_order.py
+++ sale_stock_medical_prescription_us/models/procurement_order.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2016 LasLabs Inc.
+# Copyright 2016-2017 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
 
 class ProcurementOrder(models.Model):
+
     _inherit = 'procurement.order'
 
     ndc_id = fields.Many2one(
