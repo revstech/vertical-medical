b9acaa162ad1aee1487efcb288843c6359026676
Oleg Bulkin <o.bulkin@gmail.com>
2017-05-26 12:36:23 -0700

    [10.0][FIX] medical_prescription_state_verify: Move field * Move base.kanban.stage field is_verified from parent module medical_prescription_state

---
+++ medical_prescription_state_verify/models/base_kanban_stage.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class BaseKanbanStage(models.Model):
+    _inherit = 'base.kanban.stage'
+
+    is_verified = fields.Boolean(
+        string='Verified?',
+    )
