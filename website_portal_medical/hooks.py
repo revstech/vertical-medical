b4a94d7657b1fa8f629ce60b40e0a1fd4b78b821
Dave Lasley <dave@laslabs.com>
2017-08-01 16:04:19 -0700

    [FIX] website_portal_medical_insurance: Skip hook on non-demo * Skip the legal rep hook when not in demo mode

--- website_portal_medical/hooks.py
+++ website_portal_medical/hooks.py
@@ -2,7 +2,12 @@
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+import logging
+
 from odoo import api, SUPERUSER_ID
+
+
+_logger = logging.getLogger(__name__)
 
 
 def _update_patients_legal_rep(cr, registry):
@@ -10,9 +15,14 @@ def _update_patients_legal_rep(cr, registry):
         env = api.Environment(cr, SUPERUSER_ID, {})
         patient_model = env['medical.patient']
 
-        patients = patient_model.search([
-            ('parent_id', '=', env.ref('base.partner_demo').id)
-        ])
-        patients.write(
-            {'parent_id': env.ref('portal.demo_user0_res_partner').id}
-        )
+        try:
+            patients = patient_model.search([
+                ('parent_id', '=', env.ref('base.partner_demo').id)
+            ])
+            patients.write(
+                {'parent_id': env.ref('portal.demo_user0_res_partner').id}
+            )
+        except ValueError:
+            _logger.debug(
+                'Demo partner not found - skipping the fix for it.',
+            )
