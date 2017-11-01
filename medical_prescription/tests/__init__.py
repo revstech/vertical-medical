985587020c687bbaf81f2173d305435256edb245
Brett Wood <bwood@laslabs.com>
2017-11-01 15:49:17 -0700

    [ADD] medical_prescription: Add rx_line smart button to patient view.

--- medical_prescription/tests/__init__.py
+++ medical_prescription/tests/__init__.py
@@ -2,5 +2,6 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+from . import test_medical_patient
 from . import test_medical_prescription_order
 from . import test_medical_prescription_order_line
