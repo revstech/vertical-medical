9bad609aa68f7bdc40af57a4701bbad4277966c1
Dave Lasley <dave@laslabs.com>
2018-02-09 09:23:23 -0800

    [DMCA] Add copyright headers & ReadMe statements * Add copyright headers on files that have not been entirely rewritten * Add modified work statements on modules that evolved from GNUHealth

--- medical_medication/models/medical_patient.py
+++ medical_medication/models/medical_patient.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2004 Tech-Receptives
+# Copyright 2008 Luis Falcon <lfalcon@gnusolidario.org>
 # Copyright 2016 LasLabs Inc.
 # License GPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
